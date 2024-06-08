e382f784b07123d10c7e05ae2cfc74518e745ec7
公彦赤屋先 <kimihikoakaya@gmail.com>
2024-06-08 12:35:43 +0200

    Nya

--- src/commands.js
+++ src/commands.js
@@ -3,12 +3,14 @@
  * and registration.
  */
 
-export const AWW_COMMAND = {
-  name: 'awwww',
-  description: 'Drop some cuteness on this channel.',
-};
-
-export const INVITE_COMMAND = {
-  name: 'invite',
-  description: 'Get an invite link to add the bot to your server',
+export const MEOW_COMMAND = {
+  name: 'meow',
+  description: 'Meows.',
+  options: [
+    {
+      name: 'meows',
+      description: 'How many meows to meow',
+      type: 4,
+    },
+  ],
 };
